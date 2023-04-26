acde3108d481781742c63ce9eee354a145d522fd
k-walsh <kiera_walsh@brown.edu>
2023-04-26 14:49:52 -0400

    data validation split

--- code/run.py
+++ code/run.py
@@ -2,26 +2,33 @@ import hyperparameters as hp
 from datetime import datetime
 import tensorflow as tf
 from models import VGGModel
-from preprocess import load_train_validation_data, load_test_data
+from preprocess import split_train_validation_data, load_test_data
 
-def train(model, train_data, train_labels, init_epoch):
+def train(model, train_data, train_labels, val_data, val_labels, init_epoch):
     print("inside train")
+
+    # shuffle training data 
+    rand_i = tf.random.shuffle(range(len(train_labels)))
+    train_data = tf.gather(train_data, rand_i)
+    train_labels = tf.gather(train_labels, rand_i)
+
     model.fit(
-        # x=train_data,
-        # validation_data=test_data,
-        train_data, train_labels,
+        x=train_data, 
+        y=train_labels,
+        validation_data=(val_data, val_labels),
         epochs=hp.num_epochs,
         batch_size=None,            # Required as None as we use an ImageDataGenerator; see preprocess.py get_data()
         callbacks=None,
         initial_epoch=init_epoch,
     )
 
-def test(model, test_data):
+def test(model, test_data, test_labels):
     """ Testing routine. """
 
     # Run model on test set
     model.evaluate(
         x=test_data,
+        y=test_labels,
         verbose=1,
     )
 
@@ -30,8 +37,9 @@ def main():
     timestamp = time_now.strftime("%m%d%y-%H%M%S")
     init_epoch = 0
 
-    train_data, train_labels = load_train_validation_data()
+    train_data, train_labels, val_data, val_labels = split_train_validation_data()
     print(f"{len(train_data)} training samples loaded")
+    print(f"{len(val_data)} validation samples loaded")
 
     test_data, test_labels = load_test_data()
     print(f"{len(test_data)} testing samples loaded")
@@ -48,8 +56,10 @@ def main():
         loss=model.loss_fn,
         metrics=["accuracy"])
     
-    train(model, train_data, train_labels, init_epoch)
-
+    train(model, train_data, train_labels, val_data, val_labels, init_epoch)
     print("done training")
+
+    test(model, test_data, test_labels)
+    print("done testing")
 
 main()
