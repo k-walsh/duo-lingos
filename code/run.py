12406529fa8624ec915116fce54f83c993f7eb6e
Joan Nekoye <joannekoye@gmail.com>
2023-04-26 16:48:07 -0400

    tensorboard refactoring

--- code/run.py
+++ code/run.py
@@ -1,21 +1,32 @@
+import os
 import hyperparameters as hp
 from datetime import datetime
 import tensorflow as tf
 from models import VGGModel
-from preprocess import split_train_validation_data, load_test_data
+from tensorboard_utils import \
+        ImageLabelingLogger, CustomModelSaver
+from preprocess import Datasets
 
-def train(model, train_data, train_labels, val_data, val_labels, init_epoch):
-    print("inside train")
+def train(model, datasets, checkpoint_path, logs_path, init_epoch):
 
     # shuffle training data 
-    rand_i = tf.random.shuffle(range(len(train_labels)))
-    train_data = tf.gather(train_data, rand_i)
-    train_labels = tf.gather(train_labels, rand_i)
+    rand_i = tf.random.shuffle(range(len(datasets.train_labels)))
+    train_data = tf.gather(datasets.train_data, rand_i)
+    train_labels = tf.gather(datasets.train_labels, rand_i)
 
     # shuffle val data
-    rand_i_val = tf.random.shuffle(range(len(val_labels)))
-    val_data = tf.gather(val_data, rand_i_val)
-    val_labels = tf.gather(val_labels, rand_i_val)
+    rand_i_val = tf.random.shuffle(range(len(datasets.val_labels)))
+    val_data = tf.gather(datasets.val_data, rand_i_val)
+    val_labels = tf.gather(datasets.val_labels, rand_i_val)
+
+    callback_list = [
+        # tf.keras.callbacks.TensorBoard(
+        #     log_dir=logs_path,
+        #     update_freq='batch',
+        #     profile_batch=0),
+        # ImageLabelingLogger(logs_path, datasets),
+        # CustomModelSaver(checkpoint_path, hp.max_num_weights)
+    ]
 
     model.fit(
         x=train_data, 
@@ -23,17 +34,17 @@ def train(model, train_data, train_labels, val_data, val_labels, init_epoch):
         validation_data=(val_data, val_labels),
         epochs=hp.num_epochs,
         batch_size=None,            # Required as None as we use an ImageDataGenerator; see preprocess.py get_data()
-        callbacks=None,
+        callbacks=callback_list,
         initial_epoch=init_epoch,
     )
 
-def test(model, test_data, test_labels):
+def test(model, datasets):
     """ Testing routine. """
 
     # Run model on test set
     model.evaluate(
-        x=test_data,
-        y=test_labels,
+        x=datasets.test_data,
+        y=datasets.test_labels,
         verbose=1,
     )
 
@@ -42,12 +53,20 @@ def main():
     timestamp = time_now.strftime("%m%d%y-%H%M%S")
     init_epoch = 0
 
-    train_data, train_labels, val_data, val_labels = split_train_validation_data()
-    print(f"{len(train_data)} training samples loaded")
-    print(f"{len(val_data)} validation samples loaded")
+    datasets = Datasets()
+    print("train data shape", datasets.train_data.shape)
+    print("test data shape", datasets.test_data.shape)
+    print("val data shape", datasets.val_data.shape)
+    print("train labels shape", datasets.train_labels.shape)
 
-    test_data, test_labels = load_test_data()
-    print(f"{len(test_data)} testing samples loaded")
+    checkpoint_path = "checkpoints" + os.sep + "vgg_model" + os.sep + timestamp + os.sep
+    logs_path = "logs" + os.sep + "vgg_model" + os.sep + timestamp + os.sep
+    os.makedirs(checkpoint_path)
+
+    print(f"{len(datasets.train_data)} training samples loaded")
+    print(f"{len(datasets.val_data)} validation samples loaded")
+
+    print(f"{len(datasets.test_data)} testing samples loaded")
 
     model = VGGModel()
     model(tf.keras.Input(shape=(200, 200, 3)))
@@ -61,10 +80,11 @@ def main():
         loss=model.loss_fn,
         metrics=["accuracy"])
     
-    train(model, train_data, train_labels, val_data, val_labels, init_epoch)
+    train(model, datasets, checkpoint_path, logs_path, init_epoch)
+
     print("done training")
 
-    test(model, test_data, test_labels)
+    test(model, datasets)
     print("done testing")
 
 main()
